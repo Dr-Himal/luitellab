31c556590f750404aebd279ff95a286c5713fb8c
Dr-Himal <154537421+Dr-Himal@users.noreply.github.com>
2024-01-18 05:52:30 +0545

    Update lab.py

--- lab.py
+++ lab.py
@@ -46,6 +46,7 @@ with col1:
     st.write("Fax: +977-56-526014")
     st.write("Google Scholar: https://scholar.google.com/citations?user=PJcQIW0AAAAJ&hl=en")
     st.write("Pubmed: https://pubmed.ncbi.nlm.nih.gov/?term=Himal+luitel&sort=date&size=50")
+    st.write("Research Gate: https://www.researchgate.net/profile/Himal-Luitel")
     st.write("ORCID: https://orcid.org/0000-0003-3844-0469")
     st.write("📫", "hluitel@afu.edu.np")
 
